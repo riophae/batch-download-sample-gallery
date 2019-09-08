958d7be0ea5681dcdcd143100a4167a76e741d83
Fangzhou Li <riophaelee@gmail.com>
2019-09-08 15:37:23 +0800

    细节更新

--- utils/start-aria2.js
+++ utils/start-aria2.js
@@ -1,8 +1,11 @@
 const execa = require('execa')
 const Aria2 = require('aria2')
-const sleep = require('yaku/lib/sleep')
 const getPort = require('get-port')
+const portUsed = require('port-used')
 const readConfig = require('./read-config')
+
+const CHECK_PORT_RETRY_INTERVAL = 50
+const CHECK_PORT_TIMEOUT = 5000
 
 module.exports = async function startAria2() {
   const config = readConfig()
@@ -16,14 +19,20 @@ module.exports = async function startAria2() {
     `--split=${config.aria2.split}`,
     '--conditional-get',
     '--remote-time',
-  ])
-  await sleep(500)
+  ]).catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
+  await portUsed.waitUntilUsed(
+    port,
+    CHECK_PORT_RETRY_INTERVAL,
+    CHECK_PORT_TIMEOUT,
+  )
 
   const aria2 = new Aria2({
     host: 'localhost',
     port,
   })
-
   await aria2.open()
 
   return [ aria2, port ]
